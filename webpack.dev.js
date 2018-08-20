51e7619b824a7ff9c74a626e7bbac3bac892092d
Adrian <adriandinca2008@yahoo.com>
2018-08-20 10:54:39 +0300

    Fix: Set mode to 'development'

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,21 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+const { DefinePlugin } = require('webpack');
 
 module.exports = merge(commonConfig, {
-    mode: 'production',
+    mode: 'development',
 
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
+
+    plugins: [
+        new DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
 
     devServer: {
         contentBase: './dist'
